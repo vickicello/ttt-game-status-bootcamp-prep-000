4a8c53980d8335e68821de21bf6796b1121f6fd1
Vicki Hamm <vicki.cello@gmail.com>
2018-05-12 03:44:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,5 +16,5 @@ WIN_COMBINATIONS = [
   
   def won?(board)
   WIN_COMBINATIONS.each do | win_combination |
-    if board[win_combination[0]]
+    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
     
