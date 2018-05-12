09ad907303e4d898498f6d57d0c50fce3e418678
Vicki Hamm <vicki.cello@gmail.com>
2018-05-12 03:31:01 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,20 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+  ]
+  
+
+
+  def won?(board)
+  if WIN_COMBINATIONS = false
+    nil
+  else 
