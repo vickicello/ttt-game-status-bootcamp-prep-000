e98e834a9c21956a5196adf7e6cd8a7b44606ac9
Vicki Hamm <vicki.cello@gmail.com>
2018-05-12 04:06:55 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,9 +19,9 @@ WIN_COMBINATIONS = [
      win_index_1 = win_combination[0]
      win_index_2 = win_combination[1]
      win_index_3 = win_combination[2]
-     position_1 = board[win_combination[0]]
-     position_2 = board[win_combination[1]]
-     position_3 = board[win_combination[2]]
+     position_1 = board[win_index_1]
+     position_2 = board[win_index_2]
+     position_3 = board[win_index_3]
     if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
       return win_combination
     end
